abd6aacdc59868775fa598e871d02c4a6b4d9190
liuyueyuan <liuyueyuan@zrfilm.com>
2017-12-19 17:21:26 +0800

    调整MIMIN目录

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
 import App from './App'
 import router from './router'
 import { XHeader,XButton } from 'vux'
 import Page from '@/views/components/page.vue'
-import baseMixin from "./baseMixin";
+import baseMixin from "./utils/baseMixin";
 import 'flex.css'
 
 FastClick.attach(document.body)
 
 
 Vue.config.productionTip = false
+
 Vue.component('x-header',XHeader);
 Vue.component('x-button',XButton);
 Vue.component('page',Page)
