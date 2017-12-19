968266b6f78094b80b4fd89922599dc9609a32f3
liuyueyuan <liuyueyuan@zrfilm.com>
2017-12-19 17:57:49 +0800

    fix

--- src/views/test.vue
+++ src/views/test.vue
@@ -3,6 +3,20 @@
     <div slot="contain">
      <page-scroller :api='getDataList' ref='scroller' :init-query='true' noRecordText='当前账户未添加会员卡' noRecordImage  usePulldown height='-92' >
           
+       <list twoLine :title="`我是标题`" >
+          <list-item :img="`https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513599973802&di=e0ed1059c34a8eb1d89a8e8bd1c7da11&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F4b90f603738da97726166db6ba51f8198618e376.jpg`"
+          :contentTitle="`我是contentTitle`"   extra="" isLink  :link="`/`" :contentBrief="`我是contentBasdfasdfasdfasdfasdfasrief`"  >
+            </list-item>
+            <list-item :img="`https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513599973802&di=e0ed1059c34a8eb1d89a8e8bd1c7da11&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F4b90f603738da97726166db6ba51f8198618e376.jpg`"
+          :contentTitle="`我是contentTitle`"   extra="" isLink  :link="`/`" :contentBrief="`我是contentBasdfasdfasdfasdfasdfasrief`"  >
+            </list-item>
+        </list>
+        <list twoLine :title="`我是标题`" >
+          <list-item :img="`https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513599973802&di=e0ed1059c34a8eb1d89a8e8bd1c7da11&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F4b90f603738da97726166db6ba51f8198618e376.jpg`"
+          :contentTitle="`我是contentTitle`"   extra="" isLink  :link="`/`" :contentBrief="`我是contentBasdfasdfasdfasdfasdfasrief`"  >
+            </list-item>
+        </list>
+
        <list  :title="`我是标题`"  v-for="(item,index) in dataList" >
           <list-item   is-link :content="`我是标题`" >
               <div slot="subContent">我是副标题</div>
