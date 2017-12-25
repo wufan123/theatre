3f9f3155892530e72d4d045577b58c5b194e42eb
whr <357114638@qq.com>
2017-12-25 11:44:38 +0800

    场次票，发现，介绍

---
+++ src/views/components/findList/item.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <img class="find-vidio"
+         :src="img">
+    <div class="find-body">
+      <p class="title text-ellipsis-line">{{title}}</p>
+      <p class="content text-ellipsis">
+        {{content}}</p>
+    </div>
+  </div>
+</template>
+<script>
+
+  export default {
+    name: "find-item",
+    props: ['img', 'title', 'content'],
+    data(){
+      return {}
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import "~style/base-variables.less";
+
+  .find {
+    .find-vidio {
+      width: 345px;
+      height: 194px;
+      margin: 23px 15px
+    }
+    .find-body {
+      padding: 0 15px;
+      .title {
+        font-size: 16px;
+        border-left: solid @color-sub 5px;
+        font-weight: bold;
+        padding-left: 7px;
+        line-height: 15px;
+        width: 100%;
+      }
+      .content {
+        margin-top: 12px;
+        text-indent: 2em;
+      }
+    }
+  }
+</style>
+
