76c40ac5a7571107092f66e7d20d274ca87d56c5
林傅荣 <459558744@qq.com>
2017-12-26 08:53:00 +0800

    fix config

--- config/index.js
+++ config/index.js
@@ -65,8 +65,8 @@ module.exports = {
 
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: './static',
-        assetsPublicPath: './',
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',
         baseUrl:'/api',
 
         /**
