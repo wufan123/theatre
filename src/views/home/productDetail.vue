41652453e0294e5de8faf10beb392507ace33cc8
whr <357114638@qq.com>
2018-01-08 10:52:25 +0800

    登录验证码

--- src/views/home/productDetail.vue
+++ src/views/home/productDetail.vue
@@ -10,11 +10,11 @@
   import {mapState} from "vuex";
   import StoreApi from 'api/storeApi'
   export default {
-      props:['isHermes'],
-    components:{PageScroller,XButton,DetailPage},
+    props: ['isHermes'],
+    components: {PageScroller, XButton, DetailPage},
     data(){
       return {
-        data:{},
+        data: {},
       }
     },
     computed: {
@@ -22,44 +22,54 @@
     },
     methods: {
       async buy(){
-          let goodsId =  this.data.goodsId;
-          let res = await StoreApi.createGoodsOrder(this.userInfo.bindmobile,`${goodsId}:1`);
-          if(res&&res.data){
-              let orderId = res.data;
-              // 清空优惠券缓存
-              this.$store.commit("coupon/setGoodsCouponList", null);
-              this.$router.push({name:"ConfirmGoodOrder",query:{
-                goodsId,orderId
-              }})
-          }
+        let goodsId = this.data.goodsId;
+        let res;
+        try {
+          res = await StoreApi.createGoodsOrder(this.userInfo.bindmobile, `${goodsId}:1`);
+        }
+        catch (e) {
+            //todo
+        }
+        if (res && res.data) {
+          let orderId = res.data;
+          // 清空优惠券缓存
+          this.$store.commit("coupon/setGoodsCouponList", null);
+          this.$router.push({
+            name: "ConfirmGoodOrder", query: {
+              goodsId, orderId
+            }
+          })
+        }
       },
       async fetchData(){
-         let res = await StoreApi.getGoodsDetail(this.$route.query.hyGoodsId);
-          if(res&&res.data)
-          {
-              this.data =res.data.goodInfo
-          }
+        let res = await StoreApi.getGoodsDetail(this.$route.query.hyGoodsId);
+        if (res && res.data) {
+          this.data = res.data.goodInfo
+        }
       }
     }
   }
 </script>
 <style lang="less" scoped>
   @import "~style/base-variables";
-  .primeCost{
+
+  .primeCost {
     text-decoration: line-through;
   }
-  .ImgOut{
+
+  .ImgOut {
     width: 100%;
     height: 200px;
-    img{
+    img {
       height: 200px;
       width: 100%;
     }
   }
-  .mainOut{
+
+  .mainOut {
     position: relative;
     width: 100%;
-    .mainBody{
+    .mainBody {
       position: absolute;
       z-index: 100;
       background-color: @page_bg2;
@@ -67,17 +77,26 @@
       padding: 20px;
       border-radius: 5px;
       width: 295px;
-      .content{margin-top: 20px;
-        text-indent: 2em;text-align:left;width: 100%;
+      .content {
+        margin-top: 20px;
+        text-indent: 2em;
+        text-align: left;
+        width: 100%;
       }
-      .divider{position: relative;width: 100%;}
-      .dividerLine{
+      .divider {
+        position: relative;
+        width: 100%;
+      }
+      .dividerLine {
         border-bottom: dotted @color-sub 1px;
-        position: absolute;width: 100%;z-index: 2;
+        position: absolute;
+        width: 100%;
+        z-index: 2;
       }
-      .dividerTitle{
+      .dividerTitle {
         background: @page_bg2 url(../../assets/images/home/title_bg.png) center no-repeat;
-        background-size: 100%100%;z-index: 3;
+        background-size: 100% 100%;
+        z-index: 3;
         width: 128px;
         height: 34px;
         color: @color-sub;
@@ -87,11 +106,11 @@
         line-height: 34px;
         margin: 10px 0px;
       }
-      .title{
+      .title {
         font-size: 20px;
         color: @font-color;
       }
-      .price{
+      .price {
         color: @color-sub;
         font-size: 21px;
 
