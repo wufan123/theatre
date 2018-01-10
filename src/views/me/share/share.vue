cb82e8bdc8fa6bdf2f7674051d7f9c5691096ed7
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-10 16:26:24 +0800

    fix

--- src/views/me/share/share.vue
+++ src/views/me/share/share.vue
@@ -81,7 +81,7 @@ export default {
       });
     },
     ShareFriend(){
-      wx.onMenuShareQQ({
+      wx.onMenuShareAppMessage({
         title: '这是一个测试的标题', // 分享标题
         desc: '这个是分享QQ的描述信息', // 分享描述
         link: 'https://upload-images.jianshu.io/upload_images/5928779-e2548546e1a73321.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700', // 分享链接
@@ -127,6 +127,10 @@ export default {
         // 用户取消分享后执行的回调函数
         }
       });
+      wx.error(function(res){  
+            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。  
+            alert("errorMSG:"+res);  
+        });  
     }
   }
 }
