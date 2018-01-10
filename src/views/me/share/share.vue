6a7040291cdcb645eb5b4d329f1f3923e300ba45
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-10 16:35:20 +0800

    fix

--- src/views/me/share/share.vue
+++ src/views/me/share/share.vue
@@ -65,23 +65,20 @@ export default {
         },error => { console.log(error); })
     },
     ShareQQ(){
-      alert('QQ')
-      setTimeout(function () { 
-        wx.onMenuShareQQ({
-          title: '这是一个测试的标题', // 分享标题
-          desc: '这个是分享QQ的描述信息', // 分享描述
-          link: 'https://upload-images.jianshu.io/upload_images/5928779-e2548546e1a73321.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700', // 分享链接
-          imgUrl: 'https://upload-images.jianshu.io/upload_images/5928779-e2548546e1a73321.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700', // 分享图标
-          success: function () {
-          // 用户确认分享后执行的回调函数
-          alert('分享成功');
-          },
-          cancel: function () {
-            alert('你没有分享');
-          // 用户取消分享后执行的回调函数
-          }
-        });
-      },2000);
+      wx.onMenuShareQQ({
+        title: '这是一个测试的标题', // 分享标题
+        desc: '这个是分享QQ的描述信息', // 分享描述
+        link: 'https://upload-images.jianshu.io/upload_images/5928779-e2548546e1a73321.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700', // 分享链接
+        imgUrl: 'https://upload-images.jianshu.io/upload_images/5928779-e2548546e1a73321.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700', // 分享图标
+        success: function () {
+        // 用户确认分享后执行的回调函数
+        alert('分享成功');
+        },
+        cancel: function () {
+          alert('你没有分享');
+        // 用户取消分享后执行的回调函数
+        }
+      });
     },
     ShareFriend(){
       wx.onMenuShareAppMessage({
@@ -116,7 +113,9 @@ export default {
       });
     },
     ShareSina(){
-      wx.onMenuShareQZone({
+      alert('SINA')
+      setTimeout(function() {
+        wx.onMenuShareQZone({
         title: '这是一个测试的标题', // 分享标题
         desc: '这个是分享空间的描述信息', // 分享描述
         link: 'https://upload-images.jianshu.io/upload_images/5928779-e2548546e1a73321.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700', // 分享链接
@@ -130,6 +129,8 @@ export default {
         // 用户取消分享后执行的回调函数
         }
       });
+      }, 2000);
+    }
   }
 }
 </script>
