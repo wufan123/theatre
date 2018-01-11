4bde29d4cc8cee475cab04848208fe4d4459de6f
whr <357114638@qq.com>
2018-01-11 11:04:59 +0800

    分享测试

--- src/views/me/share/share.vue
+++ src/views/me/share/share.vue
@@ -37,6 +37,7 @@
 <script>
 import { XDialog,Qrcode } from 'vux'
 import theatreApi from "api/theatreApi";
+import AuthApi from "../../../api/authApi";
 export default {
   data(){
     return{
@@ -61,6 +62,7 @@ export default {
             });
       },
     ShareFriend(){
+        console.log('1111')
       wx.onMenuShareAppMessage({
         title: '这是一个测试的标题', // 分享标题
         desc: '这个是分享QQ的描述信息', // 分享描述
@@ -96,6 +98,25 @@ export default {
             this.ruleConfig = res.data[0].miscVal
           }
         },error => { console.log(error); })
+      AuthApi.getWeixinConfig().then(success => {   
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: success.data.appId, // 必填，公众号的唯一标识
+          timestamp: success.data.timestamp, // 必填，生成签名的时间戳
+          nonceStr: success.data.noncestr, // 必填，生成签名的随机串
+          signature: success.data.signature,// 必填，签名，见附录1
+          jsApiList: [
+            'chooseWXPay',
+            'onMenuShareQQ',
+            'onMenuShareAppMessage',
+            'onMenuShareQZone',
+            'onMenuShareTimeline',
+            'scanQRCode'
+          ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        })
+      }, error => {
+
+      })
     },
     ShareQQ(){
       wx.onMenuShareQQ({
