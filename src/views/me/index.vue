800d7a83830d52d7bb32ebd5dd14bb4d328f0ccf
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-17 13:43:03 +0800

    bug修复

--- src/views/me/index.vue
+++ src/views/me/index.vue
@@ -3,7 +3,10 @@
       <div slot="contain" class="me">
           <div class="me-top">
             <span class="bold f16">{{userInfo.userNickname}}</span>
-            <p><em>余额：￥{{userInfo.userMoney}}</em>  丨  <em>积分：{{userInfo.integral}}</em></p>
+            <p><em v-if="!userInfo.memberCardBalance" >余额：￥{{userInfo.userMoney}}</em>
+               <em v-if="userInfo.memberCardBalance" >会员卡余额：￥{{userInfo.memberCardBalance.accBalance}}</em>
+              丨  <em>积分：{{userInfo.integral}}</em></p>
+            
             <i class="member f12">剧坊会员</i>
           </div>
           <div class="me-order">
