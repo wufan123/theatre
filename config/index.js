5302914ece272cb7fe78408c68cbbb810714103f
whr <357114638@qq.com>
2018-01-18 13:46:24 +0800

    修改资源路径

--- config/index.js
+++ config/index.js
@@ -61,12 +61,12 @@ module.exports = {
 
     build: {
         // Template for index.html
-        index: path.resolve(__dirname, '../dist/preJufang/index.html'),
+        index: path.resolve(__dirname, '../dist/index.html'),
 
         // Paths
-        assetsRoot: path.resolve(__dirname, '../dist/preJufang'),
+        assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: './',
         baseUrl:'/preapi',
 
         /**
