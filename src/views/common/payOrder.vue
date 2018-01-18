6ab6f85a0355514f1a3a6491bd793aa31008e328
whr <357114638@qq.com>
2018-01-18 15:12:29 +0800

    修改错误提示

--- src/views/common/payOrder.vue
+++ src/views/common/payOrder.vue
@@ -143,7 +143,7 @@
                   // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                   this.$router.push({name: 'paySuccess'})
                 } else {
-                  ctx.$util.showRequestErro({text:res.errMsg});
+                  ctx.$util.showRequestErro({text:res.err_msg});
                 }
               }
             );
