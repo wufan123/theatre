80133875a26b9e9786afb980c8944d21e0abaf23
whr <357114638@qq.com>
2018-01-19 18:36:18 +0800

    去掉调试代码

--- src/views/common/payOrder.vue
+++ src/views/common/payOrder.vue
@@ -176,7 +176,7 @@
                 "paySign": wxpay.paySign//微信签名
               },
               function (res) {
-                ctx.erroInfo = res;
+//                ctx.erroInfo = res;
                 try {
                   res = JSON.parse(res);
                 } catch (e) {
