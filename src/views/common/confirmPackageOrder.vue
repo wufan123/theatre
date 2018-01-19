4d0217448dda07332b280c644b714af07ac97544
whr <357114638@qq.com>
2018-01-19 15:09:03 +0800

    错误信息

--- src/views/common/confirmPackageOrder.vue
+++ src/views/common/confirmPackageOrder.vue
@@ -96,7 +96,7 @@
                   // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                   this.$router.push({name: 'PaySuccess'})
                 } else {
-                  ctx.$util.showRequestErro({text: res.des});
+                  ctx.$util.showRequestErro({text: res});  
                 }
               }
             );
