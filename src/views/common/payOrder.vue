cc420aa00043ad72b5b8e2bd0e304d2887b5d003
whr <357114638@qq.com>
2018-01-19 14:34:23 +0800

    微信支付结果处理

--- src/views/common/payOrder.vue
+++ src/views/common/payOrder.vue
@@ -7,7 +7,6 @@
         <group title="选择支付方式">
           <radio title="title" :options="typeData" v-model="value" disabled></radio>
         </group>
-
         <!--<div >
          <label>选择支付方式</label>
          <div class="payItem">
@@ -153,7 +152,7 @@
 
                 }
                 if (res.err_msg == "get_brand_wcpay_request:ok") {//cancel
-                  // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                  ctx.erroInfo.type='gotoPaySuccess';
                   this.$router.push({name: 'PaySuccess'})
                 } else {
                   if (res.des)
