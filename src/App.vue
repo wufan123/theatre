c2596a3b4b8b3b82c1fe998b4570cc2ac9951736
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-20 14:42:32 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
   export default {
     name: 'app',
     mounted() {
-      this.initWx();
+      //this.initWx();
     },
     computed: {
       ...mapState('common', ['openId'])
--- src/views/home/sessionDetail.vue
+++ src/views/home/sessionDetail.vue
@@ -30,6 +30,9 @@
             </div>
           </div>
             <div class="h10"></div>
+            <div class="h10"></div>
+            <div class="h10"></div>
+            <div class="h10" style="border:1px solid red"></div>
         </div>
         <x-button class="no-radius bottomBtn" type="primary" @click.native="show=true">马上购买</x-button>
       </div>
