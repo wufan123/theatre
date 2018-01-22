77d9c15bb424553952bb20f043bfa9c9d0a9f2b1
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-22 14:01:52 +0800

    test

--- src/views/common/confirmOrder.vue
+++ src/views/common/confirmOrder.vue
@@ -6,10 +6,9 @@
           <list twoLine>
             <div v-if="orderInfo&&orderInfo.film" class="good-content">
               <list-item :img="orderInfo.film.image" extra="">
-                <div slot="contentTitle" flex="main:justify" style="border:1px solid red;">
-                  <label class="text-ellipsis-line">{{orderInfo.film.filmName}}</label>
+                <div slot="contentTitle" flex="main:justify" style="border:1px solid red">
+                  <label>{{orderInfo.film.filmName}}</label>
                   <label class="o-price">￥{{orderInfo.film.price}}</label>
-                  <label >￥{{orderInfo.film.price}}</label>
                 </div>
                 <div slot="contentBrief" flex="main:justify">
                   <label class="f12">{{new Date(orderInfo.film.startTime * 1000).format('MM-dd hh:mm')}} 场</label>
