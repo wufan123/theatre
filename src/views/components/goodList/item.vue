a9092097eb752049e21a34f880e3790d47062faa
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-22 14:16:01 +0800

    test

--- src/views/components/goodList/item.vue
+++ src/views/components/goodList/item.vue
@@ -39,7 +39,7 @@
       height: 68px;
       margin-left: 15px;
     }
-    .content{margin-left: 10px;overflow: hidden;margin-right: 10px;  flex: 1;
+    .content{margin-left: 10px;overflow: hidden;margin-right: 10px;border:1px solid red;  flex: 1;
       .title{
         margin-left: 5px;
         font-size: 15px;
