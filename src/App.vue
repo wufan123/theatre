44fc394dc89ff40784fe3c4f37a2a0fc870a2457
whr <357114638@qq.com>
2018-01-23 18:46:23 +0800

    路径解析

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
         if (configRes && configRes.data) {
           //获取openid
           if (!this.openId) {
-            let code = this.$util.getQueryString('code')||this.route.query.code;//vue-router的query获取会有问题 
+            let code = this.$util.getQueryString('code')||this.$route.query.code;//vue-router的query获取会有问题
             let reUrl = 'https://jufang.zmaxfilm.com/';
             if (!code) {
               window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${configRes.data.appId}&redirect_uri=${reUrl}&response_type=code&scope=snsapi_base#wechat_redirect`
