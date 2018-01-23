d5cf5c909dfb54a90c6045f22b24ce33c66d4fa8
liuyueyuan <liuyueyuan@zrfilm.com>
2018-01-23 13:37:15 +0800

    snack

--- src/views/components/itemNumber.vue
+++ src/views/components/itemNumber.vue
@@ -1,6 +1,6 @@
 <template>
   <div flex="dir:left" class="num">
-    
+    {{numberItem.num}}
     <x-number v-show="numberItem.num>0" :value="numberItem.num" :min="0" @on-change="change" :max="parseInt(numberItem.stock)"></x-number>
     <a v-show="!numberItem.num" @click="numberItem.num++"
        class="vux-number-selector vux-number-selector-plus">
