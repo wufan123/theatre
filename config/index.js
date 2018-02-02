c32659de5ab700e10d99635626df2925f8c90d44
liuyueyuan <liuyueyuan@zrfilm.com>
2018-02-02 10:23:09 +0800

    webpack配置修改

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ module.exports = {
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
-        baseUrl:'/api',
+        baseUrl:'/preapi',
 
         /**
          * Source Maps
