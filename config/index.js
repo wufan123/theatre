01331d628b33d4b5fc61cbc732aed13bff915b1f
liuyueyuan <liuyueyuan@zrfilm.com>
2018-02-05 08:51:33 +0800

    webpack

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ module.exports = {
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
-        baseUrl:'/preapi',
+        baseUrl:'/api',
 
         /**
          * Source Maps
